6ae91bc69fe3a1f97c37ae9e32be4ea2bd729280
MoustafaGhanemm <maghanem@uci.edu>
2024-09-09 03:34:50 -0700

    Done

--- src/train.py
+++ src/train.py
@@ -9,12 +9,13 @@ from model import load_model
 import os
 from tqdm import tqdm
 from data_preprocessing import dataload_create
+from evaluate import calculate_accuracy,calculate_loss
 # Define loss function and optimizer
 
 def train(num_epochs, train_loader, val_loader, model, device):
-    num_epochs = 10
     criterion = nn.BCEWithLogitsLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.0002)
+
     for epoch in range(num_epochs):
         model.train()
         running_loss = 0.0
@@ -26,16 +27,16 @@ def train(num_epochs, train_loader, val_loader, model, device):
 
             optimizer.zero_grad()
             outputs = model(inputs)
-            loss = criterion(outputs, labels)
+            loss = calculate_loss(outputs, labels, criterion)
             loss.backward()
             optimizer.step()
 
-            running_loss += loss.item()
-            all_preds.extend((outputs > 0.5).cpu().numpy())  # Convert outputs to binary predictions
+            running_loss += loss
+            all_preds.extend(torch.sigmoid(outputs).cpu().numpy())
             all_labels.extend(labels.cpu().numpy())
 
         train_loss = running_loss / len(train_loader)
-        train_accuracy = accuracy_score(all_labels, all_preds)
+        train_accuracy = calculate_accuracy(all_preds, all_labels)
 
         model.eval()
         val_loss = 0.0
@@ -46,20 +47,16 @@ def train(num_epochs, train_loader, val_loader, model, device):
             for inputs, labels in tqdm(val_loader, desc=f"Validation Epoch {epoch+1}/{num_epochs}"):
                 inputs, labels = inputs.to(device), labels.to(device).unsqueeze(1)
                 outputs = model(inputs)
-                loss = criterion(outputs, labels)
-                val_loss += loss.item()
-                val_preds.extend((outputs > 0.5).cpu().numpy())  # Convert outputs to binary predictions
+                loss = calculate_loss(outputs, labels, criterion)
+                val_loss += loss
+                val_preds.extend(torch.sigmoid(outputs).cpu().numpy())
                 val_labels.extend(labels.cpu().numpy())
 
         val_loss /= len(val_loader)
-        val_accuracy = accuracy_score(val_labels, val_preds)
+        val_accuracy = calculate_accuracy(val_preds, val_labels)
 
         print(f'Epoch {epoch + 1}/{num_epochs}')
         print(f'Train Loss: {train_loss:.4f}, Train Accuracy: {train_accuracy:.4f}')
         print(f'Val Loss: {val_loss:.4f}, Val Accuracy: {val_accuracy:.4f}')
-
-
-
-
     # Save the fine-tuned model
     torch.save(model.state_dict(), 'resnet18_gender_classifier.pth')
