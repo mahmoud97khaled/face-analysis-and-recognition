39c69e6f26d0ec02dbd26250ff17cd84fdf5686d
ahmeddarwesh02 <101249000+engdarwesh@users.noreply.github.com>
2024-09-09 20:14:15 +0300

    model

--- tests/emotion classification test.py
+++ tests/emotion classification test.py
@@ -1,19 +1,15 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader, Dataset
 import torchvision.transforms as transforms
 from torchvision import models
-from sklearn.metrics import accuracy_score
 import cv2
-import os
-from tqdm import tqdm
 import numpy as np
-from PIL import Image
+
+device = "cpu"
 
 
 def load_model(model_path, device):
-    model = models.resnet34(weights=None)
+    model = models.resnet34(weights=None)  
     num_ftrs = model.fc.in_features
     model.fc = nn.Sequential(
         nn.Dropout(0.5),
@@ -29,22 +25,35 @@ def load_model(model_path, device):
 
     model.load_state_dict(torch.load(model_path, map_location=device))
     model = model.to(device)
-    model.eval()  
+    model.eval() 
     return model
 
 
-def preprocess_image(image_path):
+def preprocess_image(image):
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    model = load_model(model_path, device)
+
+
+    img_tensor = preprocess_image(photo)
+
+    
+
+    image = image.astype(np.float32) / 255.0
+
     transform_test = transforms.Compose([
-        transforms.Grayscale(num_output_channels=3),
+        transforms.ToPILImage(), 
+        transforms.Grayscale(num_output_channels=3),  
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
     
 
-    img = Image.open(image_path)
-    img_tensor = transform_test(img).unsqueeze(0)  
+    img_tensor = transform_test(image).unsqueeze(0)
     return img_tensor
+
 
 def predict(model, img_tensor, device):
     img_tensor = img_tensor.to(device)
@@ -53,8 +62,7 @@ def predict(model, img_tensor, device):
     with torch.no_grad():
         outputs = model(img_tensor)
 
-
-    probabilities = torch.nn.functional.softmax(outputs, dim=1)  # Get probabilities from logits
+    probabilities = torch.nn.functional.softmax(outputs, dim=1)  
     confidence, predicted_idx = torch.max(probabilities, 1)
     predicted_idx = predicted_idx.item()
     confidence = confidence.item()
@@ -66,16 +74,12 @@ def predict(model, img_tensor, device):
     return predicted_emotion, confidence
 
 
-image_path = 'me.jpg' 
-model_path = 'models/emotion_model.pth'  
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-image = cv2.imread(image_path)
+image_path = 'me.jpg'  
+model_path = 'res34_res34.pth'  
 
 
-model = load_model(model_path, device)
+photo = cv2.imread(image_path)
 
-
-img_tensor = preprocess_image(image_path)
 
 
 predicted_emotion, confidence = predict(model, img_tensor, device)
